5e55a5b95e602c7de9b1a6602a4bca82cbf49a0d
andrew-kuczynski <akucz@me.com>
2024-03-28 21:30:04 -0400

    add deploy, update copy

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,96 @@
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
-import Paper from "@mui/material/Paper";
 import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
 
 import { useCallback, useState } from "react";
 import ProgressCondition from "./components/ProgressCondition";
 
-const DURATION_SEC = 6;
-
+const DURATION_SEC = 2;
 
 // random number 1-3
 const getRandomCondition = () => {
 	return Math.floor(Math.random() * 3) + 1;
 };
 
-
 function StartView({ onStart }) {
+	return (
+		<div>
+			<p>
+				We are conducting user experience research on a new technology for
+				loading video content on the web.
+			</p>
+			<p>
+				This demo and survey should take less a minute, please give it your
+				undivided attention for the duration.
+			</p>
+			<p>Thank you!</p>
 
-
-	return <div>
-
-		<p>
-			We are conducting user experience research on a new technology for loading video content on the web. 
-		</p>
-		<p>
-			This demo and survey should take less a minute, please give it your undivided attention for the duration.
-		</p>
-		<p>
-			Thank you!
-		</p>
-
-		<div style={{
-			textAlign: 'center',
-			paddingTop: 12
-		}}>
-			<Button variant="contained" onClick={onStart}>
-				Start
-			</Button>
+			<div
+				style={{
+					textAlign: "center",
+					paddingTop: 12,
+				}}
+			>
+				<Button variant="contained" onClick={onStart}>
+					Start
+				</Button>
+			</div>
 		</div>
-	</div>
+	);
 }
 
 function LoadingCompleteView({ onContinue }) {
+	return (
+		<div>
+			<p>Loading is complete.</p>
+			<p>No need to watch the loaded video. Please continue</p>
 
-	return (<div>
-
-		<p>
-			Loading complete
-		</p>
-
-		<div style={{
-			textAlign: 'center',
-			paddingTop: 12
-		}}>
-			<Button variant="contained" onClick={onContinue}>
-				Continue
-			</Button>
+			<div
+				style={{
+					textAlign: "center",
+					paddingTop: 12,
+				}}
+			>
+				<Button variant="contained" onClick={onContinue}>
+					Continue
+				</Button>
+			</div>
 		</div>
-	</div>)
+	);
 }
 
 function SurveyView({ condition }) {
-
 	const surveyLinks = {
-		1: 'https://google.com',
-		2: 'https://google.com',
-		3: 'https://google.com'
-	}
+		1: "https://google.com",
+		2: "https://google.com",
+		3: "https://google.com",
+	};
 
-	return (<div>
-		<p>
-			The demo is complete. Please help us collect data by completing the <Link href={surveyLinks[condition]}>short survey</Link>.
-		</p>
-		<p>
-			Thank you again.
-		</p>
-	</div>)
+	return (
+		<div>
+			<p>
+				The demo is complete. Please help us collect data by completing the{" "}
+				<Link href={surveyLinks[condition]}>short survey</Link>.
+			</p>
+			<p>Thank you again.</p>
+		</div>
+	);
 }
-
-
 
 function App() {
 	const [state, setState] = useState(() => {
 		if (localStorage.getItem("DONE")) {
-			return "SURVEY"
+			return "SURVEY";
 		}
-		return "START"
+		return "START";
 	});
 
 	const [condition] = useState(() => {
 		const localCondition = localStorage.getItem("condition");
-		if (localCondition && ['1', '2', '3'].includes(localCondition)) {
-			return parseInt(localCondition);
+		if (localCondition && ["1", "2", "3"].includes(localCondition)) {
+			return Number.parseInt(localCondition);
 		}
 		const rand = getRandomCondition();
 		localStorage.setItem("condition", rand.toString());
@@ -104,19 +102,21 @@ function App() {
 	}, []);
 
 	const onContinue = useCallback(() => {
-		localStorage.setItem("DONE", "true")
-		setState("SURVEY")
+		localStorage.setItem("DONE", "true");
+		setState("SURVEY");
 	}, []);
 
 	const view = {
 		START: <StartView onStart={() => setState("LOADING")} />,
 		LOADING: (
-			<div style={{
-				display: 'flex',
-				alignItems: 'center',
-				justifyContent: 'center',
-				height: '100%'
-			}}>
+			<div
+				style={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					height: "100%",
+				}}
+			>
 				<ProgressCondition
 					condition={condition}
 					duration={DURATION_SEC}
@@ -147,15 +147,22 @@ function App() {
 						padding: 16,
 						maxHeight: "100%",
 						height: 500,
-						fontSize: 22
+						fontSize: 22,
 					}}
 				>
-				<div style={{ display: 'flex', gap: 8, justifyContent: 'center', fontSize: 14 }}>
-					<div>CAP5108</div>
-					<div>-</div>
-					<div>University of Florida</div>
-				</div>
-				{view[state]}
+					<div
+						style={{
+							display: "flex",
+							gap: 8,
+							justifyContent: "center",
+							fontSize: 14,
+						}}
+					>
+						<div>CAP5108</div>
+						<div>-</div>
+						<div>University of Florida</div>
+					</div>
+					{view[state]}
 				</Paper>
 			</Container>
 		</>
